4fec31c7fee75162da986168c8578017c56c197d
Marco Baringer <mb@bese.it>
2006-02-10 10:57:55 +0100

    Fix IS macro's handling of the default (unknown) case
    In the list-match-case form we were using _? (which only matches the
    symbol _?) instead of ?_ (which matches anything), so the default
    clause was never being used.

--- src/check.lisp
+++ src/check.lisp
@@ -147,10 +147,10 @@ Wrapping the TEST form in a NOT simply preducse a negated reason string."
          (setf bindings (list (list v ?value))
                effective-test `(,?satisfies ,v)
                default-reason-args (list "~S did not satisfy ~S" v `',?satisfies)))
-        (_?
+        (?_
          (setf bindings '()
                effective-test test
-               default-reason-args (list "No reason supplied."))))
+               default-reason-args (list "No reason supplied"))))
       `(let ,bindings
          (if ,effective-test
              (add-result 'test-passed :test-expr ',test)
