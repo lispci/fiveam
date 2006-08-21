b37e3f37f173eea9c8149109f73f9b34c6a1515c
Henrik Hjelte <henrik@evahjelte.com>
2006-08-21 10:29:48 +0200

    Platform-independent pathnames in make-qbook

--- docs/make-qbook.lisp
+++ docs/make-qbook.lisp
@@ -1,8 +1,13 @@
 (asdf:oos 'asdf:load-op :FiveAM)
 (asdf:oos 'asdf:load-op :qbook)
 
-(asdf:oos 'qbook:publish-op :fiveam
+(asdf:oos 'qbook:publish-op :FiveAM
           :generator (make-instance 'qbook:html-generator
                                     :title "FiveAM"
-                                    :output-directory "./html/"))
+                                    :output-directory
+                                    (merge-pathnames
+                                        (make-pathname :directory '(:relative "docs" "html"))
+                                        (asdf:component-pathname (asdf:find-system :FiveAM)))))
+
+          
 
