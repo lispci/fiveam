7df749a8a0381171ab005a3f284385ce39757277
Luca Capello <luca@pca.it>
2007-10-21 22:10:56 +0200

    add an (eval-when ...) when defining a test suite

--- t/suite.lisp
+++ t/suite.lisp
@@ -2,7 +2,8 @@
 
 (in-package :it.bese.fiveam)
 
-(unless (get-test :it.bese)
-  (def-suite :it.bese))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (get-test :it.bese)
+    (def-suite :it.bese)))
 
 (def-suite :it.bese.fiveam :in :it.bese)
