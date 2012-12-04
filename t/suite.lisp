4abaf749ed6e3b694a39385417ca52d80033ad6e
Kiyoshi Mizumaru <https://github.com/kmizumar>
2012-12-04 11:47:52 +0100

    Use correct case for package name and suite name (for AllegroCL modern mode).

--- t/suite.lisp
+++ t/suite.lisp
@@ -1,9 +1,9 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 
-(in-package :it.bese.fiveam)
+(in-package :it.bese.FiveAM)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (unless (get-test :it.bese)
     (def-suite :it.bese)))
 
-(def-suite :it.bese.fiveam :in :it.bese)
+(def-suite :it.bese.FiveAM :in :it.bese)
